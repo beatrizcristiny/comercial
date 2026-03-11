1a97098fc03ad1698b0012de7ef0cf0f4f927667
beatrizcristiny <beatrizcrispires@gmail.com>
2026-03-11 19:44:33 -0300

    Parte do html

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validação de login usando PHP</title>
+</head>
+<body>
+    <h1>Formulário</h1>
+    <form method="POST">
+        Usuário <br>
+        <input type="text" name="usuario"><br><br>
+        
+        Senha <br>
+        <input type="passaword" name="senha"><br><br>
+        <input type="submit" value="Entrar">
+</form>
+        
+        
+
+</body>
+</html>
+
 <?php
-hfsfafj
 ?>
